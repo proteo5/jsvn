207694911969ccded0efdbe31db0a75d7ab1071e
proteo5 <proteo5@hotmail.com>
2014-09-28 03:18:07 -0700

    Primary functionality working

---
+++ proteo5.JSvn.Concept1/proteo5.JSvn.Concept1/app/index.js
@@ -0,0 +1,35 @@
+﻿app.modules.index = {
+    "module": "index",
+    "name": "Home Page",
+    "Description": "This is our default page",
+    "version": "0.1.0.0001",
+    "html": [
+        {
+            "type": "h1",
+            "attributes": "",
+            "content": "This is the main Title"
+        },
+        {
+            "type": "div",
+            "id": "div1",
+            "attributes": "style='color:blue;'",
+            "content": "test div 1"
+        },
+        {
+            "type": "div",
+            "id": "div2",
+            "attributes": "style='color:red;'",
+            "content": [{
+                "type": "p",
+                "attributes": "",
+                "content": "test p tag"
+            }, ]
+        }
+    ],
+    "code": {
+        start: function () {
+            app.renderModule(app.modules.index.html);
+        }
+    }
+}
+app.modules.index.code.start();
---
+++ proteo5.JSvn.Concept1/proteo5.JSvn.Concept1/app/main.js
@@ -0,0 +1,19 @@
+﻿app.modules.main = {
+	"app": "Concept JSvn",
+    "title": "Concept JSvn",
+	"description": "This is a Concept application for Javascript View Notation",
+    "version": "0.1.0.0001",
+    "author": "Alfredo Pinto Molina",
+    "porletsPath": "Scripts/JSvn/Lib",
+	"layout": null,
+	"initialModule": "index",
+	"initialTag": "main-content",
+	code: {
+		start: function () {
+			var title = app.render("{{title}} - {{version}}", app.modules.main);
+			$('title').text(title);
+			app.loadModule(app.modules.main.initialModule);
+		}
+	}
+};
+app.modules.main.code.start();
