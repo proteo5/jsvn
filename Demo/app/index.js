97ae5407eab24341112453d69c267dbb8c879afe
proteo5 <proteo5@hotmail.com>
2014-09-30 03:38:38 -0700

    Updated demo

--- Demo/app/index.js
+++ Demo/app/index.js
@@ -2,32 +2,35 @@
     module: "index",
     name: "Home Page",
     description: "This is our default page",
-    view: [
-        {
-            "porlet": "h1",
-            "content": "This is the main Title"
-        },
-        {
-            "porlet": "div",
-            "id": "div1",
-            "style": "color:blue;",
-            "content": "test div 1"
-        },
-        {
-            "porlet": "div",
-            "id": "div2",
-            "style": "color:red;",
-            "content": [{
-                "porlet": "h2",
-                "style": "color:green;",
-                "content": "This is the h2 Title"
+    view: {
+        viewName: 'index',
+        view: [
+            {
+                "porlet": "h1",
+                "content": "This is the main Title"
             },
             {
-                "porlet": "p",
-                "content": "test p tag"
-            }]
-        }
-    ],
+                "porlet": "div",
+                "id": "div1",
+                "style": "color:blue;",
+                "content": "test div 1"
+            },
+            {
+                "porlet": "div",
+                "id": "div2",
+                "style": "color:red;",
+                "content": [{
+                    "porlet": "h2",
+                    "style": "color:green;",
+                    "content": "This is the h2 Title"
+                },
+                {
+                    "porlet": "p",
+                    "content": "test p tag"
+                }]
+            }
+        ]
+    },
     code: {
         start: function () {
             app.view(app.modules.index.view);
