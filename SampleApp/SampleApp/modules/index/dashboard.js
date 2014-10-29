89fc8b286a2c87190fdaa976851f8bddb710ea03
proteo5 <proteo5@hotmail.com>
2014-10-28 18:08:25 -0700

    app Sample

---
+++ SampleApp/SampleApp/modules/index/dashboard.js
@@ -0,0 +1,20 @@
+﻿app.modules.index_dashboard = {
+    module: "Dashboard",
+    name: "Dashboard",
+    type: "control",
+    description: "This is the dashboard page",
+    code: {
+        start: function () {
+            app.view({
+                view: {
+                    "viewName": "dashboard",
+                    "description": "This is the view for the dashboard",
+                    "viewType": "html",
+                    "viewPath": "index/",
+                    "elements": []
+                },
+                place: app.settings.moduleTag
+            });
+        }
+    }
+}
---
+++ SampleApp/SampleApp/modules/logIn.js
@@ -0,0 +1,20 @@
+﻿app.modules.logIn = {
+    module: "logIn",
+    name: "Log In",
+    type: "page",
+    description: "This is the page to Log in to the application",
+    code: {
+        start: function () {
+            app.view({
+                view: {
+                    "viewName": "logIn",
+                    "description": "This view is used when a users sites is called",
+                    "viewType": "json",
+                    "viewPath": "",
+                    "elements": []
+                },
+                place: app.settings.bodyTag
+            });
+        }
+    }
+}
